363c60ee3d4b3813d0f8091e6123904c7592689b
thinklim <sagalim3983@gmail.com>
2020-01-14 23:22:00 +0900

    createBlogBaseTemplate
    블로그 앱에서 공통된 Template 부분을 Base Template으로 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('<str:username>', MemberBlogView.as_view(), name='member_blog'),
-    path('add', add_blog, name='add'),
+    path('add/', add_blog, name='add'),
     path('', BlogView.as_view(), name='blog'),
 ]
